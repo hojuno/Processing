f6546dbd3aafbdf77a25964d1b0907771f8974fe
hojuno <reo-jun@naver.com>
2020-09-11 14:46:52 +0900

    10724 호준
    ^^

---
+++ sketch_200911b/sketch_200911b.js
@@ -0,0 +1,18 @@
+function setup() {
+ createCanvas(500, 500);
+ fill(255, 100, 100);
+ ellipse(100, 100, 50,50);
+ fill(10, 255, 155);
+ ellipse(150, 150, 50,50);
+ fill(5, 3, 67);
+ ellipse(150, 100, 50,50);
+ fill(100,100,100);
+ ellipse(100, 150, 50,50);
+ fill(44,44,44);
+ ellipse(195,125, 50,50)
+}
+
+
+function draw() {
+
+}
